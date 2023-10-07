5a4d0b696fb7db2ea7c53ade57bdbdaeea9b176e
Rahul Rajput <104721504+rahulrajput-rr@users.noreply.github.com>
2023-10-07 19:34:11 +0530

    Add files via upload

---
+++ DecisionAndIteration/If-else/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible for voting.");
+        } else {
+            System.out.println("You are not eligible for voting.");
+        }
+        scanner.close();
+
+    }
+}
