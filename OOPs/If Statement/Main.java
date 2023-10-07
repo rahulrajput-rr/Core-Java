085db9a0881e0c11b602d20561848f0a80ea6e5b
Rahul Rajput <104721504+rahulrajput-rr@users.noreply.github.com>
2023-10-07 19:24:33 +0530

    Add files via upload

---
+++ OOPs/If Statement/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible for voting.");
+        }
+        scanner.close();
+
+    }
+}
